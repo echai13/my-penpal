340bab377a54ca1f2b927e18646735c4ea60ffc7
echai13 <cher13@brandeis.edu>
2018-01-10 19:08:04 -0500

    changes before deployment

--- client/components/index.js
+++ client/components/index.js
@@ -3,9 +3,9 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export { default as Main } from './main'
-export { default as UserHome } from './user-home'
-export { Login, Signup } from './auth-form'
+export { default as Main } from './Main'
+export { default as UserHome } from './UserHome'
+export { Login, Signup } from './AuthForm'
 export { default as Inbox } from './Inbox'
 export { default as Preferences } from './Preferences'
 export { default as Penpals } from './Penpals'
